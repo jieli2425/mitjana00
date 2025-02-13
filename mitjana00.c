f0e7970371482b8492e2eedf99754a00bc8d2c1a
Davidvamosfull <jieli3837@gmail.com>
2025-02-13 19:54:44 +0100

    d

--- mitjana00.c
+++ mitjana00.c
@@ -24,7 +24,7 @@ int main() {
 	mitjana=suma/numero_repeticions;
 	printf("La mitjana resultant és: %.2f\n",mitjana);
 	printf("La mitjana ha estat trobada amb èxit\n");
-	printf("La mitjana resultant és: %.2f\n",mitjana)0;
+	printf("La mitjana resultant és: %.2f\n",mitjana);
 	printf("Operació finalitzada amb èxit\n");	
 	return 0;
 }
